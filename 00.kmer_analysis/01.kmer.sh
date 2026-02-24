d89740a64f1f8c15abf7199ee492df1d2543965c
Liangkun Wang <wangliangkun501@gmail.com>
2026-02-24 17:01:21 +0800

    Update 01.kmer.sh

--- 00.kmer_analysis/01.kmer.sh
+++ 00.kmer_analysis/01.kmer.sh
@@ -1,3 +1,3 @@
-ls /home/wangliangkun/sucker/sra/data/wgs_R*.fq.gz | awk '{print "gzip-dc "$0 }' > generate.file
+ls /home/wangliangkun/sucker/sra/data/wgs_R*.fq.gz | awk '{print "gzip -dc "$0 }' > generate.file
 jellyfish count -t 10 -C -m 21 -s 1G -g generate.file -G 2 -o Kmer_21
 jellyfish histo -o Kmer_21.stat Kmer_21 -t 10 -h 10000000 -o Kmer_21.histo Kmer_21
