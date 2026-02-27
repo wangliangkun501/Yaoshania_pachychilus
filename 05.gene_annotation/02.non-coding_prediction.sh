c1d97d0eba4f37eccfb5352cc926b9bde71fcd34
Liangkun Wang <wangliangkun501@gmail.com>
2026-02-27 15:01:27 +0800

    Update 02.non-coding_prediction.sh

--- 05.gene_annotation/02.non-coding_prediction.sh
+++ 05.gene_annotation/02.non-coding_prediction.sh
@@ -1,1 +1,10 @@
+## tRNA
+tRNAscan-SE  -E  --thread 10  -o  tRNA.results  --gff  tRNA.gff3 -f  tRNA.structures \
+-m tRNA.summary ../Yp.genome.fasta 1>tRNAscan-SE.log 2>&1
 
+## rRNA
+
+
+## other ncRNAs
+cmscan --cut_ga --rfam --nohmmonly --cpu 40 -Z 451.3 --tblout genome_rfam.tblout \
+--fmt 2 --clanin ./Rfam.clanin ./Rfam.cm ../Yp.genome.fasta > genome_rfam.cmscan 2>genome_rfam.cmscan.log
